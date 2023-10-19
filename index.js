514744864917d706322051629903e0c31e0ee55d
temitopeohassan <temitope.hassan.2@gmail.com>
2023-10-19 10:13:08 +0100

    files reset

--- index.js
+++ index.js
@@ -1,71 +1,145 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-require('dotenv').config()
-require('./models/db');
-const userRouter = require('./routes/user');
-const Users = require('./models/user');
-const Place = require('./models/place')
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(cors())
 app.use(express.json())
-app.use(requestLogger)
-app.use(express.static('build'))
-app.use(userRouter);
+app.use(express.static('public'));
 
 let places = [
-]
+  {
+    id: 1,
+    type: 'IRISH PUB',
+    title: 'Waxy O\' Connors\'s London',
+    startingTime: '2023-09-21 15:00',
+    image: 'event_1.png',
+    description:
+      'Roomy, relaxed pub with regular live music & bar food, plus sports events on the big screen.',
+      location:{
+        latitude: '51.511600',
+          longitude: '-0.133100',
+          latitudeDelta: '0.005',
+          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
+        },
+    },
+  {
+    id: 2,
+    type: 'AFRICAN RESTAURANT',
+    title: 'Enish',
+    startingTime: '2023-09-25 15:00',
+    image: 'event_2.png',
+    description:
+      'With 11 thriving branches across the UK & two in Dubai, Enish Restaurant has been serving authentic cuisine for almost decade Preparing dishes from the diverse culture of Nigeria, Enish brings you a real taste of the country and a special twist to some of your favourite dishes.',
+      location:{
+        latitude: '51.4839',
+          longitude: '-0.06635',
+          latitudeDelta: '0.005',
+          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
+        },
+    },
+   {
+    id: 3,
+    type: 'PUB',
+    title: 'The Royal Oak',
+    startingTime: '2023-10-21 11:00',
+    image: 'event_3.png',
+    description:
+      'Traditional Victorian corner pub with tiled exterior, specialising in cask and real ales.',
+      location:{
+        latitude: '51.5005',
+          longitude: '-0.0916',
+          latitudeDelta: '0.005',
+          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
+        },
+    },
+  {
+    id: 4,
+    type: 'AFRICAN RESTAURANT',
+    title: 'Akoko',
+    startingTime: '2023-08-25 12:00',
+    image: 'event_4.png',
+    description:
+      'Elevated West African dishes are paired with international wines at this warm, upmarket restaurant.',
+      location:{
+        latitude: '51.516994',
+          longitude: '-0.136538',
+          latitudeDelta: '0.005',
+          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
+        },
+    },
+   {
+    id: 5,
+    type: 'BAR & LOUNGE',
+    title: 'Ku Lounge',
+    startingTime: '2023-09-21 09:10',
+    image: 'event_5.png',
+    description:
+      'Stylish cocktail lounge bar with banquettes, contemporary chandeliers and pendant lights.',
+      location:{
+        latitude: '51.512071',
+          longitude: '-0.129809',
+          latitudeDelta: '0.005',
+          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
+        },
+    },
+];
 
+let Users = [
+  {
+      id: 1, 
+      email: 'user1@email.com',
+      username: 'user1', 
+      password: 'password', 
+      userToken: 'token123',
+      firstName:'Maurice',
+      lastName: 'Mickleson',
+      phoneNumber: '1029384756',
+      county: 'London',
+      country: 'United Kingdom',
+      cover: 'cover1.png',
+      avatar: 'avatar1.png'
+  },
+  {
+      id: 2, 
+      email: 'user2@email.com',
+      username: 'user2', 
+      password: 'pass1234', 
+      userToken: 'token12345',
+      firstName:'Mariam',
+      lastName: 'Goodhew',
+      phoneNumber: '9876543210',
+      county: 'Gloucester',
+      country: 'United Kingdom',
+      cover: 'cover2.png',
+      avatar: 'avatar2.png'
+  },
+  {
+      id: 3, 
+      email: 'testuser@email.com',
+      username: 'testuser', 
+      password: 'testpass', 
+      userToken: 'testtoken',
+      firstName:'Thomas',
+      lastName: 'Anderson',
+      phoneNumber: '1234567890',
+      county: 'Surrey',
+      country: 'United Kingdom',
+      cover: 'cover3.png',
+      avatar: 'avatar3.png'
+  },
+];
 
-app.get('/api/places', (request, response) => {
-  Place.find({}).then(places => {
-    response.json(places)
-  })
+app.get('/', (req, res) => {
+  res.send('<h1>Where2goo Backend</h1>')
 })
 
-app.post('/api/places', (request, response) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const place = new Place({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  place.save().then(savedPlace => {
-    response.json(savedPlace)
-  })
+app.get('/api/places', (req, res) => {
+  res.json(places)
 })
 
-app.get('/api/places/:id', (request, response) => {
-  Place.findById(request.params.id).then(place => {
-    response.json(place)
-  })
+
+app.get('/api/users', (req, res) => {
+  res.json(Users)
 })
 
-app.delete('/api/places/:id', (request, response) => {
-  const id = Number(request.params.id)
-  places = places.filter(place => place.id !== id)
-
-  response.status(204).end()
-})
-
-app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
