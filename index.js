c74d2d68cd9efa9baddd2a437158c18da67098da
temitopeohassan <temitope.hassan.2@gmail.com>
2023-12-07 10:14:46 +0100

    places to mysql

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
 });
 
+async function getPlaces() {
+  const connection = await pool.getConnection();
+  try {
+    const [rows] = await connection.query("SELECT * FROM places");
+    return rows;
+  } finally {
+    connection.release();
+  }
+}
+
  async function getUsers() {
   const connection = await pool.getConnection();
   try {
@@ -48,97 +58,20 @@ async function insertUser(email, fullName, password) {
 async function fetchData() {
   const users = await getUsers();
 
-  const userId = 2; // Replace with the desired user ID
-  const singleUser = await getSingleUser(userId);
-}
+ }
 
 // Call the async function
 fetchData();
 
-let places = [
-  {
-    id: 1,
-    type: 'IRISH PUB',
-    title: 'Waxy O\' Connors\'s London',
-    startingTime: '2023-09-21 15:00',
-    image: 'event_1.png',
-    description:
-      'Roomy, relaxed pub with regular live music & bar food, plus sports events on the big screen.',
-      location:{
-        latitude: '51.511600',
-          longitude: '-0.133100',
-          latitudeDelta: '0.005',
-          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
-        },
-    },
-  {
-    id: 2,
-    type: 'AFRICAN RESTAURANT',
-    title: 'Enish',
-    startingTime: '2023-09-25 15:00',
-    image: 'event_2.png',
-    description:
-      'With 11 thriving branches across the UK & two in Dubai, Enish Restaurant has been serving authentic cuisine for almost decade Preparing dishes from the diverse culture of Nigeria, Enish brings you a real taste of the country and a special twist to some of your favourite dishes.',
-      location:{
-        latitude: '51.4839',
-          longitude: '-0.06635',
-          latitudeDelta: '0.005',
-          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
-        },
-    },
-   {
-    id: 3,
-    type: 'PUB',
-    title: 'The Royal Oak',
-    startingTime: '2023-10-21 11:00',
-    image: 'event_3.png',
-    description:
-      'Traditional Victorian corner pub with tiled exterior, specialising in cask and real ales.',
-      location:{
-        latitude: '51.5005',
-          longitude: '-0.0916',
-          latitudeDelta: '0.005',
-          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
-        },
-    },
-  {
-    id: 4,
-    type: 'AFRICAN RESTAURANT',
-    title: 'Akoko',
-    startingTime: '2023-08-25 12:00',
-    image: 'event_4.png',
-    description:
-      'Elevated West African dishes are paired with international wines at this warm, upmarket restaurant.',
-      location:{
-        latitude: '51.516994',
-          longitude: '-0.136538',
-          latitudeDelta: '0.005',
-          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
-        },
-    },
-   {
-    id: 5,
-    type: 'BAR & LOUNGE',
-    title: 'Ku Lounge',
-    startingTime: '2023-09-21 09:10',
-    image: 'event_5.png',
-    description:
-      'Stylish cocktail lounge bar with banquettes, contemporary chandeliers and pendant lights.',
-      location:{
-        latitude: '51.512071',
-          longitude: '-0.129809',
-          latitudeDelta: '0.005',
-          longitudeDelta: '0.005 * (SIZES.width / SIZES.height)',
-        },
-    },
-];
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Where2goo Backend</h1>')
 })
 
-app.get('/api/places', (req, res) => {
-  res.json(places)
+app.get('/api/places',async (req, res) => {
+  const places = await getPlaces();
+  res.json(places);
 })
 
 
